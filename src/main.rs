0ee6347535d265dd7e94482f5982b1289b88042f
Alifya53 <alifyahussain53@gmail.com>
2020-04-28 00:10:51 +0500

    code  commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+// use futures::executor::block_on;
+use std::time::Duration;
+fn dance_1(){
+    thread::sleep(Duration::from_secs(3));
+    println!("I am Dancing on the song");  
+}
+fn main(){
+fn dance(){
+    thread::sleep(Duration::from_secs(5));
+    println!("I am Dancing on the song on dance function"); 
+}    
+learn_song();
+sing_song();
+dance(); 
+dance_1();
+
+}
+fn sing_song() {
+    thread::sleep(Duration::from_secs(2));
+    println!("I am singing the song");
+}
+fn learn_song(){
+    thread::sleep(Duration::from_secs(2));
+    println!("I am learning the song");
 }
