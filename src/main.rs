ada3b62209edcf5d20f90e9393c9ca17053fa4a1
Alifya53 <alifyahussain53@gmail.com>
2020-04-28 01:17:28 +0500

    code updated commit

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,46 @@
 //     println!("I am learning the song");
 // }
 
+// use futures::executor::block_on;
+// use std::thread;
+// use std::time::Duration;
+
+// async fn IR_SENSOR()-> bool {
+//     thread::sleep(Duration::from_secs(2));
+//     println!{"sending data from IR sensor..."};
+    
+//     true
+// }
+// async fn ultrasonic_sensor()-> i32{
+//     thread::sleep(Duration::from_secs(5));
+//     println!{"sending data from ultrasonic..."};
+//    22
+// }
+// async fn combining_data(){
+//     // let IR = IR_SENSOR();
+//     // let ultra = ultrasonic_sensor();
+//     let s1 = IR_SENSOR().await;
+//     let s2 = ultrasonic_sensor().await;
+//     // futures::join(s1,s2);
+//     println!{"Final data {:#?} {:#?}",s1,s2};
+// }
+// fn main() {
+// block_on(combining_data());
+// }
+
+use async_std::task;
 use futures::executor::block_on;
 use std::thread;
 use std::time::Duration;
 
 async fn IR_SENSOR()-> bool {
-    thread::sleep(Duration::from_secs(2));
+    task::sleep(Duration::from_secs(2)).await;
     println!{"sending data from IR sensor..."};
     
     true
 }
 async fn ultrasonic_sensor()-> i32{
-    thread::sleep(Duration::from_secs(5));
+    task::sleep(Duration::from_secs(5)).await;
     println!{"sending data from ultrasonic..."};
    22
 }
